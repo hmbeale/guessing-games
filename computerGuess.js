4b43a5bba09a6903418d4263000ff36db488fb3d
Holden Beale <itdbealh@aikencounty.net>
2019-03-14 16:27:04 -0400

    minor refactoring

--- computerGuess.js
+++ computerGuess.js
@@ -7,7 +7,7 @@ const highBound = 500;
 console.log (`please think of a number from ${lowBound} to ${highBound}, inclusive`);
 console.log (`is your number ${Math.round((lowBound + highBound/2))}?`);
 console.log (`if yes, please type \'y\'`);
-console.log (`if no, please type \'h\' for me to guess higher and ` +
+console.log (`if no, please type \'h\' for me to guess higher or ` +
              `\'l\' for me to guess lower`);
 
 let lowGuess = lowBound -1;
@@ -15,17 +15,22 @@ let highGuess = highBound + 1;
 let guess = Math.round((lowGuess + highGuess)/2);
 
 stdin.addListener("data", function(d) {
-    if (d.trim() === 'l'){
-      console.log('ah, I should guess lower');
-      highGuess = guess;
+    //takes 'l' or 'h'
+    //updates guessing parameters based on input
+    const computerGuess = (higherOrLower) => {
+      higherOrLower === 'h' ? console.log ('ah, so I should guess higher') :
+                              console.log ('ah, so I should guess lower') ;
+      higherOrLower === 'h' ? lowGuess = guess : highGuess = guess;
+      console.log(`your number must be between ${lowGuess} and ${highGuess}`);
       guess = Math.round((lowGuess + highGuess)/2);
-      console.log(`is your number ${guess}?`)
+      console.log(`is your number ${guess}?`);
+    }
+
+    if (d.trim() === 'l'){
+      computerGuess('l');
     }
     if (d.trim() === 'h'){
-      console.log('ah I should guess higher');
-      lowGuess = guess;
-      guess = Math.round((lowGuess + highGuess)/2);
-      console.log(`is your number ${guess}?`)
+      computerGuess('h');
     }
     if (d.trim() === 'y'){
       console.log('woo! I got it.');
